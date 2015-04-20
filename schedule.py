f2f9737dd1111a23d467cdcdc813391d664d1d6d
Peter Hadlaw <hadlawp@gmail.com>
2015-04-20 17:53:09 -0500

    Skeleton

---
+++ schedule.py
@@ -0,0 +1,16 @@
+
+
+class Scheduler(object):
+
+    def __init__(self, num_machines, jobs):
+        return
+
+    def schedule(self):
+        pass
+
+
+def read_input(filename):
+    pass
+
+if __name__ == '__main__':
+    pass
