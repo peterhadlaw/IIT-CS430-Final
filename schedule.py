d63e138e88a657cc434db502a9300d34bf0e6115
Henry Winn <henrywinn@henrywinn.com>
2015-04-20 18:02:39 -0500

    read_input() complete
    Returns a tuple with [0] as number of jobs and [1] as an array of tuples.
    Each tuple is a job with start and end time

--- schedule.py
+++ schedule.py
@@ -8,9 +8,21 @@ class Scheduler(object):
     def schedule(self):
         pass
 
-
 def read_input(filename):
-    pass
+	# returns tuple
+	# (num_machines,[(job_start,finish),...])
+	f = open(filename)
+	machines = f.readline()
+	machines = int(machines)
+	jobs = []
+	while True:
+		line = f.readline()
+		if not line: break
+		job = line.split()
+		job = (int(job[0]),int(job[1]))
+		jobs.append(job)
+	return (machines,jobs)
+
 
 if __name__ == '__main__':
-    pass
+	pass
