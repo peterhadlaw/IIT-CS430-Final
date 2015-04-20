2b75efb028d574eb2be6cd377f42b8f4cd49cfb4
Peter Hadlaw <hadlawp@gmail.com>
2015-04-20 18:10:46 -0500

    Minor formatting updates

--- schedule.py
+++ schedule.py
@@ -9,20 +9,22 @@ class Scheduler(object):
     def schedule(self):
         pass
 
+
 def read_input(filename):
-	# returns tuple
-	# (num_machines,[(job_start,finish),...])
-	f = open(filename)
-	machines = f.readline()
-	machines = int(machines)
-	jobs = []
-	while True:
-		line = f.readline()
-		if not line: break
-		job = line.split()
-		job = (int(job[0]),int(job[1]))
-		jobs.append(job)
-	return (machines,jobs)
+    # returns tuple
+    # (num_machines,[(job_start,finish),...])
+    f = open(filename)
+    machines = f.readline()
+    machines = int(machines)
+    jobs = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        job = line.split()
+        job = (int(job[0]), int(job[1]))
+        jobs.append(job)
+        return (machines, jobs)
 
 
 if __name__ == '__main__':
