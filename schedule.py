0e31be549dff0924627f59d0dfb69363afff6845
Peter Hadlaw <hadlawp@gmail.com>
2015-04-20 23:09:47 -0500

    Full documentation for read input and minor tweaks

--- schedule.py
+++ schedule.py
@@ -11,19 +11,36 @@ class Scheduler(object):
 
 
 def read_input(filename):
-    # returns tuple
-    # (num_machines,[(job_start,finish),...])
+    """
+    Read the specified input file, return number of machines and list of jobs.
+    Returned data format is a tuple containg the number of machines,
+    and a list of tuples containing job start and end times.
+
+    Arguments:
+        filename - Path to input file. First line is the number of machines, all
+                subsequent lines are jobs "JOB_START_TIME JOB_END_TIME".
+
+                Example:
+
+                3               <- number of machines
+                0 3             <- job starting at 0, ending at 3
+                0 1             <- job starting at 0, ending at 1
+                1 2             <- job starting at 1, ending at 2
+                1 4             <- job starting at 1, ending at 4
+
+    Return Value:
+        Note: job listing is in no particular order
+        (number_machines, [(job_start, job_end), (job_start, job_end), ...])
+    """
     f = open(filename)
-    machines = f.readline()
-    machines = int(machines)
+    machines = int(f.readline())
     jobs = []
     while True:
         line = f.readline()
         if not line:
             break
         job = line.split()
-        job = (int(job[0]), int(job[1]))
-        jobs.append(job)
+        jobs.append((int(job[0]), int(job[1])))
     return (machines, jobs)
 
 
